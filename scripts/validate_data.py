39832a78f43858a0f7c18048b892264bff158324
MountAye <45800208+MountAye@users.noreply.github.com>
2025-12-05 23:27:50 -0600

    renew data schema and tested

--- scripts/validate_data.py
+++ scripts/validate_data.py
@@ -1,57 +1,80 @@
+import json
 import yaml
 from pathlib import Path
 from jsonschema import validate,FormatChecker,ValidationError
 
 schema_str = """
-type: object
-properties:
-  title:    {'type': 'string', 'minLength': 1}
-  excerpt:  {'type': 'string', 'minLength': 1}
-  tags:     {'type': 'array', 'items': { 'type': 'string', 'minLength': 1} }
-  data:
-    type: array
-    items:
-      type: object
-      minItems: 1
-      properties:
-        title:  {'type': 'string', 'minLength': 1}
-        author: {'type': 'string', 'minLength': 1}
-        date:   {'type': 'string', 'minLength': 1, 'format': 'date'}
-        tags:   {'type': 'array', 'items': {'type': 'string', 'minLength': 1} }
-        details: 
-          type: array
-          items:
-            type: object
-            minItems: 1
-            properties:
-              text:   {'type': 'string', 'minLength': 1}
-              author: {'type': 'string', 'minLength': 1}
-              date:   {'type': 'string', 'minLength': 1, 'format': 'date'}
-              url:    {'type': 'string', 'minLength': 1}
-              backup: {'type': 'string', 'minLength': 1}
-            required: ['text']
-        visuals:
-          type: array
-          minItems: 1
-          items:
-            type: object
-            properties:
-              alt:    {'type': 'string', 'minLength': 1}
-              url:    {'type': 'string', 'minLength': 1}
-              backup: {'type': 'string', 'minLength': 1}
-            required: ['url']
-      required: ['title']
-required: ['data']
+{
+  "type": "object",
+  "properties": {
+    "title":   { "type": "string", "minLength": 1 },
+    "excerpt": { "type": "string", "minLength": 1 },
+    "tags": {    
+      "type": "array",
+      "minItems": 1,
+      "items": { "type": "string", "minLength": 1 }
+    },
+    "data": {
+      "type": "array",
+      "minItems": 1,
+      "items": {
+        "type": "object",
+        "properties": {
+          "date":  { "type": "string", "minLength": 1, "format": "date" },
+          "author":{ "type": "string", "minLength": 1 },
+          "title": { "type": "string", "minLength": 1 },
+          "url":   { "type": "string", "minLength": 1 },
+          "backup":{ "type": "string", "minLength": 1 },
+          "tags":  {
+            "type": "array",
+            "minItems": 1,
+            "items": { "type": "string", "minLength": 1 }
+          },
+          "details": { 
+            "type": "array",
+            "minItems": 1,
+            "items": {
+              "type": "object",
+              "properties": {
+                "text":   { "type": "string", "minLength": 1 },
+                "author": { "type": "string", "minLength": 1 },
+                "date":   { "type": "string", "minLength": 1, "format": "date" },
+                "url":    { "type": "string", "minLength": 1 },
+                "backup": { "type": "string", "minLength": 1 }
+              },
+              "required": ["text"]
+            }
+          },
+          "visuals": {
+            "type": "array",
+            "minItems": 1,
+            "items": {
+              "type": "object",
+              "properties": {
+                "alt":    { "type": "string", "minLength": 1 },
+                "url":    { "type": "string", "minLength": 1 },
+                "backup": { "type": "string", "minLength": 1 }
+              },
+              "required":  ["url"]
+            }
+          }
+        },
+        "required": ["title"]
+      }
+    }
+  },
+  "required": ["data"]
+}
 """
 
 if __name__=="__main__":
-    schema = yaml.load(schema_str,Loader=yaml.BaseLoader)
-    for path_yml in Path("reviews").glob("*/*.yml"):
+    schema = json.loads(schema_str)
+    for path_yml in Path("reviews/").glob("**/*.yml"):
+        print(str(path_yml))
         with open(str(path_yml),"r") as file_yml:
             yml = yaml.load(file_yml,Loader=yaml.BaseLoader)
         try:
             validate(yml,schema,format_checker=FormatChecker())
         except ValidationError as e:
-            raise Exception(f"In {path_yml}:\n{str(e)}")
-    
+            raise Exception(f"<validate_data>: In {path_yml}:\n{str(e)}")
     print("All YAML files under reviews/ have passed basic syntax check.")
